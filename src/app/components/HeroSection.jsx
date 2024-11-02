7abb891be7039e5111db3f42d90338df66f7129a
aswincs37 <aswincs37@gmail.com>
2024-11-02 20:52:17 +0530

    moified2

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -52,7 +52,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-             href="public/myCv.pdf"
+             href="/myCv.pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
               download // This attribute prompts a download
             >
