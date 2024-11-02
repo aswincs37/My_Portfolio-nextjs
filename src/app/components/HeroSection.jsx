d60fe126f9cc2e3d883a8df3f680e3f6a1cee829
aswincs37 <aswincs37@gmail.com>
2024-11-02 21:31:49 +0530

    last_modify

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -54,7 +54,7 @@ const HeroSection = () => {
 
 
            <Link
-            href="/myCv.pdf"
+            href="/myCV.pdf"
             className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             download
             >
